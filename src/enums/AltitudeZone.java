fdc9e95e0ce9f5bd4bd2a84e5c6f5862eec0cbde
Joulies <Joulies@Joul>
2015-06-24 13:04:48 +0300

    q

---
+++ src/enums/AltitudeZone.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum AltitudeZone {
+
+	UNDER_6000, BETWEEN_6000_AND_7000, BETWEEN_7000_AND_8000, OVER_8000
+
+}
--- src/model/Space.java
+++ src/model/Space.java
@@ -1,22 +1,39 @@
 package model;
 
+import java.util.ArrayList;
+
+import enums.AltitudeZone;
 import enums.SpaceSize;
 
 public class Space {
 
-	private int movementCost, acclimatizationIndicator;
+	private int movementCost, acclimatizationIndicator, victoryPoints;
+	private AltitudeZone altitudeZone = null;
 	private double centerX, centerY;
 	private SpaceSize spaceSize = null;
+	private ArrayList<Space> adjacentUp = new ArrayList<>();
+	private ArrayList<Space> adjacentDown = new ArrayList<>();
 
 	public Space(int movementCost, int acclimatizationIndicator,
-			double centerX, double centerY, SpaceSize spaceSize) {
+			int victoryPoints, AltitudeZone altitudeZone, double centerX,
+			double centerY, SpaceSize spaceSize) {
 
 		this.movementCost = movementCost;
 		this.acclimatizationIndicator = acclimatizationIndicator;
+		this.victoryPoints = victoryPoints;
+		this.altitudeZone = altitudeZone;
 		this.centerX = centerX;
 		this.centerY = centerY;
 		this.spaceSize = spaceSize;
 
+	}
+
+	public void addAdjacentUp(Space space) {
+		this.adjacentUp.add(space);
+	}
+
+	public void addAdjacentDown(Space space) {
+		this.adjacentDown.add(space);
 	}
 
 }
