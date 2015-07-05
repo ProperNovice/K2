3f9c89c4d899a92c428c6873c6ddfe35239cb2a6
Joulies <Joulies@Joul>
2015-07-05 23:08:58 +0300

    q

--- src/enums/Coordinates.java
+++ src/enums/Coordinates.java
@@ -6,7 +6,7 @@ public enum Coordinates {
 	MAP(Dimensions.GAP_BETWEEN_BORDERS.x(), Dimensions.GAP_BETWEEN_BORDERS.y()),
 	ACCLIMATIZATION_INDICATOR(MAP.x() + Dimensions.MAP_GAME.x() + Dimensions.GAP_BETWEEN_COMPONENTS.x(), Dimensions.FRAME.y() - Dimensions.GAP_BETWEEN_BORDERS.y() - Dimensions.ACCLIMATIZATION_INDICATOR_GAME.y()),
 	RISK_TOKEN_PILE(ACCLIMATIZATION_INDICATOR.x(), Dimensions.GAP_BETWEEN_BORDERS.y()),
-	DECK(RISK_TOKEN_PILE.x(), Coordinates.RISK_TOKEN_PILE.y() + 2 * Dimensions.RISK_TOKEN_GAME.y() * Ratio.RISK_TOKEN.ratio()),
+	DECK(RISK_TOKEN_PILE.x(), Coordinates.RISK_TOKEN_PILE.y() + 2 * (Dimensions.RISK_TOKEN_ORIGINAL.y() * Ratio.RISK_TOKEN.ratio() + Dimensions.GAP_BETWEEN_COMPONENTS.y())),
 	
 	;
 
