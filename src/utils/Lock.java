b8c4846422e51188aa855d0e2e97bd645d49927d
Joulies <Joulies@Joul>
2015-07-18 16:07:14 +0300

    q

--- src/utils/Lock.java
+++ src/utils/Lock.java
@@ -2,7 +2,33 @@ package utils;
 
 public class Lock {
 
-	private static Semaphore semaphore = new Semaphore(0);
+	private static Semaphore semaphore = new Semaphore();
+
+	private static class Semaphore {
+
+		private java.util.concurrent.Semaphore semaphore = null;
+
+		public Semaphore() {
+			this.semaphore = new java.util.concurrent.Semaphore(0);
+		}
+
+		public void releasePermit() {
+			this.semaphore.release();
+		}
+
+		public void acquirePermit() {
+			try {
+				this.semaphore.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public int availablePermits() {
+			return this.semaphore.availablePermits();
+		}
+
+	}
 
 	public static void lock() {
 
